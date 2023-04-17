575fa5b3f473b28b9dba48190a9fa00a36d13b79
MercedesDavila <mmdavila@outlook.com>
2023-04-16 18:36:59 -0600

    Updates

---
+++ precache-manifest.b39c3ca5980f992a094ccbb94db1d558.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23e15b38d81c113e3f8fea628bd6a226",
+    "url": "/MercedesDavila/Tinder-Clone-Reactjs/index.html"
+  },
+  {
+    "revision": "6ada5bab156c1a7930e2",
+    "url": "/MercedesDavila/Tinder-Clone-Reactjs/static/css/main.be26c36f.chunk.css"
+  },
+  {
+    "revision": "a8550d48d574966e7067",
+    "url": "/MercedesDavila/Tinder-Clone-Reactjs/static/js/2.9c2770b8.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/MercedesDavila/Tinder-Clone-Reactjs/static/js/2.9c2770b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ada5bab156c1a7930e2",
+    "url": "/MercedesDavila/Tinder-Clone-Reactjs/static/js/main.de41cd3a.chunk.js"
+  },
+  {
+    "revision": "aef45ddf34ccb0163f1a",
+    "url": "/MercedesDavila/Tinder-Clone-Reactjs/static/js/runtime-main.10c4c273.js"
+  },
+  {
+    "revision": "e680277bcb12bb66acfc1f5a3b37dc33",
+    "url": "/MercedesDavila/Tinder-Clone-Reactjs/static/media/pofile_pic.e680277b.jpg"
+  }
+]);
